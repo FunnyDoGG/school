145558120b20774c8afbac71e328a85de2b1b367
Danila <casada1918@gmail.com>
2022-10-04 00:04:36 +0300

    замена "я" на "ты"

--- school/Program.cs
+++ school/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string error = ("Всем привет, сегодня я покушал и поиграл в игры. А еще я сходил в магазин и купил продукты");
+            string correct = error.Replace("я", "ты");
+            Console.WriteLine(correct);
         }
     }
 }
